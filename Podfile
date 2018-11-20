a0f68e9adead241afa4702f702069259b89268c7
Nishanth Badavide Sooryanaray <nishanth@saal.ai>
2018-11-20 12:46:06 +0400

    Added cocapods to the project.

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+target 'Todey' do
+  use_frameworks!
+
+  # Pods for Todey
+  pod 'RealmSwift'
+
+end
---
+++ Pods/Target Support Files/Pods-Todey/Pods-Todey-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Todey : NSObject
+@end
+@implementation PodsDummy_Pods_Todey
+@end
